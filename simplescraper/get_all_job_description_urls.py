52de00c70fccb89ebbbab2fecc4dfee857743fdf
Petra Carrion <petra@carrion.io>
2021-10-02 23:42:01 +0200

    Get Listing URLs from the sitemap

--- simplescraper/get_all_job_description_urls.py
+++ simplescraper/get_all_job_description_urls.py
@@ -4,23 +4,33 @@ import xmltodict as xmltodict
 SITEMAP_INDEX_XML = "https://www.stepstone.de/5/sitemaps/de/sitemapindex.xml"
 
 
-def get_listings_url():
-    global listing_urls
+def get_listing_urls():
     response = requests.get(SITEMAP_INDEX_XML)
     parsed_response = xmltodict.parse(response.content)
     sitemap = parsed_response['sitemapindex']
     sitemap_entries = sitemap['sitemap']
-    listing_urls = []
+    urls = []
     for i in sitemap_entries:
         url = i['loc']
         print(url)
         if 'listings' in url:
-            listing_urls.append(url)
-    return listing_urls
+            urls.append(url)
+    return urls
 
 
-listing_urls = get_listings_url()
+listing_urls = get_listing_urls()
 print(listing_urls)
 
+
+def get_job_description_urls(listing_url):
+    print(f'Parsing {listing_url}')
+    response = requests.get(listing_url)
+    print(response)
+    return []
+
+
+for listing_url in listing_urls:
+    job_description_urls = get_job_description_urls(listing_url)
+    print(job_description_urls)
 
 
