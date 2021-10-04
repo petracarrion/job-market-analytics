3b87873ff14ab43eb4ae8f46a096446c854bcf7a
Petra Carrion <petra@carrion.io>
2021-10-04 23:46:41 +0200

    Make the job description download work in parallel

--- simplescraper/download_job_descriptions.py
+++ simplescraper/download_job_descriptions.py
@@ -1,43 +1,47 @@
+import asyncio
 import os
 import time
 
 import pandas as pd
-from playwright.sync_api import sync_playwright, TimeoutError
+from playwright.async_api import async_playwright, Error, TimeoutError
 
 from simplescraper.get_job_description_urls import DATA_RESULTS_URLS_CSV
 from simplescraper.utils.webclient import get_local_path
 
 
-def open_first_page(browser):
-    page = browser.new_page()
-    page.goto('https://www.stepstone.de/')
-    page.click('#ccmgt_explicit_accept')
+async def open_first_page(browser):
+    page = await browser.new_page()
+    await page.goto('https://www.stepstone.de/')
+    await page.click('#ccmgt_explicit_accept')
     time.sleep(1)
     return page
 
 
-def download_urls(urls):
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=250)
-        page = open_first_page(browser)
-        for idx, url in enumerate(urls):
-            file_path = get_local_path(url)
-            if os.path.isfile(file_path):
-                continue
-            try:
-                print(f'Downloading ({idx+1}/{len(urls)}): {url}')
-                page.goto(url)
-                listing_content = page.query_selector('.listing-content')
-                listing_content_html = listing_content.inner_html()
-                listing_content_html = listing_content_html.replace('\xad', '')
-                with open(file_path, 'w', encoding='utf-8') as f:
-                    f.write(listing_content_html)
-            except TimeoutError:
-                print(f'TimeoutError: Timeout error while requesting the page {url}')
-            except AttributeError:
-                print(f'AttributeError: it seems the following URL is gone {url}')
-
-        browser.close()
+async def download_urls(urls):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False, slow_mo=250)
+        try:
+            page = await open_first_page(browser)
+            for idx, url in enumerate(urls):
+                file_path = get_local_path(url)
+                if os.path.isfile(file_path):
+                    continue
+                try:
+                    print(f'Downloading ({idx+1}/{len(urls)}): {url}')
+                    await page.goto(url)
+                    listing_content = await page.query_selector('.listing-content')
+                    listing_content_html = await listing_content.inner_html()
+                    listing_content_html = listing_content_html.replace('\xad', '')
+                    with open(file_path, 'w', encoding='utf-8') as f:
+                        f.write(listing_content_html)
+                except TimeoutError:
+                    print(f'TimeoutError: Timeout error while requesting the page {url}')
+                except AttributeError:
+                    print(f'AttributeError: it seems the following URL is gone {url}')
+        except Error:
+            print('It seems that the browser crashed.')
+        finally:
+            await browser.close()
 
 
 # def get_urls():
@@ -49,9 +53,39 @@ def download_urls(urls):
 #
 # download_urls(get_urls())
 
-df = pd.read_csv(DATA_RESULTS_URLS_CSV)
-df = df[df.job_name_slug.str.contains("Data-Engineer")]
-df = df.head(400)
-urls = df['job_url'].tolist()
-print(urls)
-download_urls(urls)
+def split_dataframe(df, chunk_size):
+    chunks = list()
+    num_chunks = len(df) // chunk_size + 1
+    for i in range(num_chunks):
+        chunks.append(df[i*chunk_size:(i+1)*chunk_size])
+    return chunks
+
+
+sem = asyncio.Semaphore(8)
+
+
+async def safe_download_urls(urls):
+    async with sem:  # semaphore limits num of simultaneous downloads
+        return await download_urls(urls)
+
+
+async def main():
+    tasks = [
+        asyncio.ensure_future(safe_download_urls(chunk['job_url'].tolist()))  # creating task starts coroutine
+        for chunk
+        in dfs
+    ]
+    await asyncio.gather(*tasks)
+
+if __name__ == '__main__':
+    df = pd.read_csv(DATA_RESULTS_URLS_CSV)
+    dfs = split_dataframe(df, 50)
+
+    # df = df[df.job_name_slug.str.contains("Entwickle")]
+    # df = df.head(500)
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main())
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
