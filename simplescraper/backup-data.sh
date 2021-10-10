3314394c8985f40dede63705ff1231a35882a6cb
Petra Carrion <petra@carrion.io>
2021-10-10 13:30:04 +0200

    Add elapsed time profiling to the bash scripts

--- simplescraper/backup-data.sh
+++ simplescraper/backup-data.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+SECONDS=0
+
 source .env
 
 filename=backup.$(date '+%Y-%m-%d').tar.gz
 tar -zcvf "${filename}" "${DATA_DIR}"
 mv "${filename}" "${BACKUP_FOLDER}"
+
+duration=$SECONDS
+echo "${0##*/}: $((duration / 60)) minutes and $((duration % 60)) seconds elapsed."
--- simplescraper/simplescraper.sh
+++ simplescraper/simplescraper.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+SECONDS=0
+
 source .env
 
 python tasks/list_downloaded_urls.py
 python tasks/download_sitemap.py
 python tasks/download_job_descriptions.py
+
+duration=$SECONDS
+echo "${0##*/}: $((duration / 60)) minutes and $((duration % 60)) seconds elapsed."
