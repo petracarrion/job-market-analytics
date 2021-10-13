e4b25c34bdaf3878e2f7be6b694571d046b50376
Petra Carrion <petra@carrion.io>
2021-10-13 23:17:27 +0200

    Normalize job descriptions

--- simplescraper/tasks/normalize_job_descriptions.py
+++ simplescraper/tasks/normalize_job_descriptions.py
@@ -10,11 +10,11 @@ def normalize_job_description(html_content):
     result = {}
     print(html_content)
     soup = BeautifulSoup(html_content)
-    first_script_tag = soup.find('script', id='js-section-preloaded-HeaderStepStoneBlock')
-    first_script_tag_lines = first_script_tag.text.split('\n')
-    for line in first_script_tag_lines:
+    script_tag = soup.find('script', id='js-section-preloaded-HeaderStepStoneBlock')
+    script_tag_lines = script_tag.text.split('\n')
+    for line in script_tag_lines:
         if line.startswith(METADATA_JSON_PREFIX) and line.endswith(METADATA_JSON_SUFFIX):
             json_line = line[len(METADATA_JSON_PREFIX):len(line) - len(METADATA_JSON_SUFFIX)]
-            result['listing_data'] = json.loads(json_line)['listingData']
+            result['listingData'] = json.loads(json_line)['listingData']
 
     return result
