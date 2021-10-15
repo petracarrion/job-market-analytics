2362aa9579beec8f16a6e6fc1fb696a91a547eda
Petra Carrion <petra@carrion.io>
2021-10-15 23:49:13 +0200

    Improve unit test

--- tests/test_normalize_job_descriptions.py
+++ tests/test_normalize_job_descriptions.py
@@ -4,9 +4,11 @@ import os
 
 from tasks.normalize_job_descriptions import normalize_job_description
 
+DATA_DIR = 'data/normalize_job_description/'
+
 
 def load_file(file_path):
-    with open('tests/data/normalize_job_description/' + file_path, 'r') as f:
+    with open(f'{DATA_DIR}{file_path}', 'r') as f:
         content = f.read()
     return content
 
@@ -21,7 +23,7 @@ def _test_normalize_job_descriptions(test_case):
 
 
 def test_normalize_job_descriptions():
-    test_cases = [os.path.splitext(os.path.basename(f))[0] for f in glob.iglob('tests/data/normalize_job_description/'
-                                                                               'input/*.html')]
+    test_cases = [os.path.splitext(os.path.basename(f))[0] for f in glob.iglob(f'{DATA_DIR}/input/*.html')]
+    assert len(test_cases) == 3
     for test_case in test_cases:
         _test_normalize_job_descriptions(test_case)
