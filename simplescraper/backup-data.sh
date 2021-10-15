1ff996c210ec151b085a09a935d1dc96a9fa5ac8
Petra Carrion <petra@carrion.io>
2021-10-15 21:40:25 +0200

    Rename BACKUP_FOLDER to BACKUP_DIR

--- simplescraper/backup-data.sh
+++ simplescraper/backup-data.sh
@@ -6,7 +6,7 @@ source .env
 
 filename=backup.$(date '+%Y-%m-%d').tar.gz
 tar -zcvf "${filename}" "${DATA_DIR}"
-mv "${filename}" "${BACKUP_FOLDER}"
+mv "${filename}" "${BACKUP_DIR}"
 
 duration=$SECONDS
 echo "${0##*/}: $((duration / 60)) minutes and $((duration % 60)) seconds elapsed."
