afdee4d5a14a0a6bd09c29d2a95970b9631253c8
Petra Carrion <petra@carrion.io>
2021-10-20 09:47:55 +0200

    Make a noise when the pipeline is done

--- simplescraper/scrape_data_source.py
+++ simplescraper/scrape_data_source.py
@@ -1,3 +1,5 @@
+import os
+
 from tasks.download_job_descriptions import download_job_descriptions
 from tasks.download_sitemap import download_sitemap
 from tasks.list_downloaded_urls import list_downloaded_urls
@@ -9,6 +11,7 @@ def scraper_data_source():
     list_downloaded_urls(job_id)
     download_sitemap(job_id)
     download_job_descriptions(job_id)
+    os.system('say -v Zuzana A je to')
 
 
 if __name__ == "__main__":
