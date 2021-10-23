37d18eed79bbbe7ddcb29cbfc1730af13f104775
Petra Carrion <petra@carrion.io>
2021-10-23 22:16:33 +0200

    Refactor

--- simplescraper/tasks/list_job_descriptions_to_download.py
+++ simplescraper/tasks/list_job_descriptions_to_download.py
@@ -1,14 +1,15 @@
+import pandas as pd
+
 from common.env_variables import LATEST_JOB_ID
 from common.storage import load_temp_df, DOWNLOADED_JOB_DESCRIPTIONS_CSV, SITEMAP_URLS_CSV, save_temp_df, \
     JOB_DESCRIPTIONS_TO_DOWNLOAD_CSV
 
 
 def list_job_descriptions_to_download(job_id, df_sitemap_urls, df_downloaded):
-    df = df_sitemap_urls
-    df = df.merge(df_downloaded, on='url', how='left', indicator=True)
-    df = df.query('_merge == "left_only"')
-    df = df.drop(columns=['_merge'])
-    df = df.reset_index(drop=True)
+    df_downloaded = df_downloaded[['url']]
+    df_downloaded = df_downloaded.drop_duplicates()
+    df = df_sitemap_urls[['url']]
+    df = pd.concat([df, df_downloaded, df_downloaded]).drop_duplicates(keep=False)
 
     save_temp_df(df, job_id, JOB_DESCRIPTIONS_TO_DOWNLOAD_CSV)
     return df
