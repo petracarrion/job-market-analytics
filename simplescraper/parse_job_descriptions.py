bd4be6f6a0755c068db60eaa9ab5e9346df6df09
Petra Carrion <petra@carrion.io>
2021-10-23 23:50:05 +0200

    Parse job descriptions

--- simplescraper/parse_job_descriptions.py
+++ simplescraper/parse_job_descriptions.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 from common.entity import JOB_DESCRIPTION
+from tasks.chunk_job_descriptions_to_parse import chunk_job_descriptions_to_parse
 from tasks.list_downloaded_job_descriptions import list_downloaded_job_descriptions
 from tasks.list_job_descriptions_to_parse import list_job_descriptions_to_parse
 from tasks.list_parsed_job_descriptions import list_parsed_job_descriptions
 from tasks.parse_job_description import parse_job_description
-from common.logging import get_logger
+from common.logging import get_logger, configure_logger
 from common.storage import get_job_id, load_raw_file, save_cleansed_df, load_cleansed_df
 
 DEBUG = True
@@ -16,9 +17,12 @@ logger = get_logger()
 def load_and_parse(row) -> str:
     timestamp = row['timestamp']
     file_name = row['file_name']
+    chunk_id = row['chunk_id']
+    pos_in_chunk = row['pos_in_chunk']
+    chunk_size = row['chunk_size']
     html_content = load_raw_file(JOB_DESCRIPTION, timestamp, file_name)
     try:
-        logger.info(f'Parsing: {timestamp}/{file_name}')
+        logger.debug(f'Parsing ({chunk_id}) {pos_in_chunk}/{chunk_size}: {timestamp}/{file_name}')
         parsed_content = parse_job_description(html_content)
         return parsed_content
     except AttributeError:
@@ -28,28 +32,30 @@ def load_and_parse(row) -> str:
 
 def parse_job_descriptions():
     job_id = get_job_id()
+    configure_logger(job_id)
     df_downloaded = list_downloaded_job_descriptions(job_id)
     df_parsed = list_parsed_job_descriptions(job_id)
-    df = list_job_descriptions_to_parse(job_id, df_downloaded, df_parsed)
-    if DEBUG:
-        df = df.sample(n=20)
-    # df = df.reset_index(drop=True)
-    df['parsed_content'] = df.apply(load_and_parse, axis=1)
-    # df = df.reset_index(drop=True)
-    df = df.join(pd.json_normalize(df['parsed_content']))
-    df = df.drop(columns=['parsed_content'])
-    df[['year', 'moth', 'day']] = df['timestamp'].str.split('-', 2, expand=True)
+    df_to_parse = list_job_descriptions_to_parse(job_id, df_downloaded, df_parsed)
+    dfs_to_parse = chunk_job_descriptions_to_parse(job_id, df_to_parse)
+    for index, df in enumerate(dfs_to_parse):
+        chunk_pos = index + 1
+        num_chunks = len(dfs_to_parse)
+        chunk_id = f'{chunk_pos}/{num_chunks}'
+        chunk_size = df.shape[0]
+        df['chunk_id'] = chunk_id
+        df = df.reset_index(drop=True)
+        df['pos_in_chunk'] = df.index + 1
+        df['chunk_size'] = chunk_size
 
-    save_cleansed_df(df, JOB_DESCRIPTION)
+        logger.info(f'Starting to parse job descriptions in a df with size: {chunk_size}')
+        df['parsed_content'] = df.apply(load_and_parse, axis=1)
+        df = df.join(pd.json_normalize(df['parsed_content']))
+        df = df.drop(columns=['parsed_content'])
+        df[['year', 'moth', 'day']] = df['timestamp'].str.split('-', 2, expand=True)
+        df = df.drop(columns=['chunk_id', 'pos_in_chunk', 'chunk_size'])
 
-    print(df)
-
-
-def read_parquet():
-    df = load_cleansed_df(JOB_DESCRIPTION)
-    print(df)
+        save_cleansed_df(df, JOB_DESCRIPTION)
 
 
 if __name__ == "__main__":
     parse_job_descriptions()
-    read_parquet()
