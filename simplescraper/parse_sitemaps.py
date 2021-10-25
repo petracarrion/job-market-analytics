91b6713e5f8bdfbb27e7d11e87f4c812c2e668bb
Petra Carrion <petra@carrion.io>
2021-10-25 21:14:14 +0200

    Parse sitemaps

--- simplescraper/parse_sitemaps.py
+++ simplescraper/parse_sitemaps.py
@@ -1,12 +1,32 @@
+import bs4
+from loguru import logger
+
+from common.entity import SITEMAP
 from common.logging import configure_logger
-from common.storage import get_run_id
+from common.storage import get_run_id, load_raw_file
 from tasks.list_downloaded_sitemaps import list_downloaded_sitemaps
+
+DEBUG = True
+
+
+def load_and_parse(row):
+    timestamp = row['timestamp']
+    file_name = row['file_name']
+    sitemap_content = load_raw_file(SITEMAP, timestamp, file_name)
+    logger.debug(f'Parsing: {timestamp}/{file_name}')
+    soup = bs4.BeautifulSoup(sitemap_content, 'lxml')
+    urls = [loc.text for loc in soup.findAll('loc')]
+    return urls
 
 
 def parse_sitemaps():
     run_id = get_run_id()
     configure_logger(run_id)
     df = list_downloaded_sitemaps(run_id)
+    if DEBUG:
+        df = df[df['timestamp'] == '2021-10-19']
+    df['url'] = df.apply(load_and_parse, axis=1)
+    df = df.explode('url')
     print(df)
 
 
