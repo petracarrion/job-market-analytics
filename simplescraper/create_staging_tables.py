a6e32783d45c2ffbfef04d207cf36d920ff77eb9
Petra Carrion <petra@carrion.io>
2021-11-12 20:33:34 +0100

    Create the staging tables in the datawarehouse dynamically

--- simplescraper/create_staging_tables.py
+++ simplescraper/create_staging_tables.py
@@ -1,12 +1,23 @@
 import psycopg2
 
 from common.entity import Entity, SITEMAP, JOB_DESCRIPTION
+from common.env_variables import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_HOST, POSTGRES_PORT
 from common.storage import list_cleansed_files
 
 PARQUET_DFW_DIR = '/var/lib/parquet-fdw/data/'
 
 
-def create_staging_tables(entity: Entity):
+def create_db_connection():
+    return psycopg2.connect(
+        database=POSTGRES_DB,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        host=POSTGRES_HOST,
+        port=POSTGRES_PORT
+    )
+
+
+def create_staging_tables(conn, entity: Entity):
     column_list = [f'{column.column_name} {column.column_type}' for column in entity.stg_columns]
     column_list = ', '.join(column_list)
 
@@ -15,7 +26,8 @@ def create_staging_tables(entity: Entity):
     file_list = sorted(file_list)
     file_list = ' '.join(file_list)
 
-    sql_stamment = f'''
+    drop_table_statmment = f'drop foreign table if exists stg_{entity.name};'
+    create_table_stamment = f'''
     create foreign table stg_{entity.name}
         (
         {column_list}
@@ -24,9 +36,15 @@ def create_staging_tables(entity: Entity):
         options (filename '{file_list}');
         '''
 
-    print(sql_stamment)
+    cursor = conn.cursor()
+    cursor.execute(drop_table_statmment)
+    cursor.execute(create_table_stamment)
+
+    conn.commit()
 
 
 if __name__ == "__main__":
-    create_staging_tables(SITEMAP)
-    create_staging_tables(JOB_DESCRIPTION)
+    _conn = create_db_connection()
+    create_staging_tables(_conn, SITEMAP)
+    create_staging_tables(_conn, JOB_DESCRIPTION)
+    _conn.close()
