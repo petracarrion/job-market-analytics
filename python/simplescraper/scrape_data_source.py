5694648a172451dda9b8a8ae6553da9dcdde96c1
Petra Carrion <petra@carrion.io>
2021-12-21 21:39:14 +0100

    Disable audio notification after the data source has been scraped

--- python/simplescraper/scrape_data_source.py
+++ python/simplescraper/scrape_data_source.py
@@ -1,10 +1,8 @@
-import os
-
 from common.logging import configure_logger
+from common.storage import get_run_id
 from tasks.download_job_descriptions import download_job_descriptions
 from tasks.download_sitemap import download_sitemap
 from tasks.list_downloaded_job_descriptions import list_downloaded_job_descriptions
-from common.storage import get_run_id
 from tasks.list_job_descriptions_to_download import list_job_descriptions_to_download
 
 
@@ -15,7 +13,8 @@ def scraper_data_source():
     df_sitemap = download_sitemap(run_id)
     df_to_download = list_job_descriptions_to_download(run_id, df_sitemap, df_downloaded)
     download_job_descriptions(run_id, df_to_download)
-    os.system('say -v Zuzana A je to')
+
+    # os.system('say -v Zuzana A je to')
 
 
 if __name__ == "__main__":
