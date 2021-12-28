4fb527f21cff32cb37fc6aea18d12ea424bc7a79
Petra Carrion <petra@carrion.io>
2021-12-28 20:36:43 +0100

    Create the Data Vault

--- sql/warehouse/models/job_market_analytics/hub_job.sql
+++ sql/warehouse/models/job_market_analytics/hub_job.sql
@@ -1,8 +1,11 @@
 {{ config(materialized='table') }}
 
-select job_id
-  from {{ source('public', 'stg_sitemap') }}
-union
-select job_id
-  from {{ source('public', 'stg_job_description') }}
+select *
+from (select job_id
+      from {{ source('public', 'stg_sitemap') }}
+      union
+      select job_id
+      from {{ source('public', 'stg_job_description') }}
+     ) as t
+where job_id is not null
 order by job_id
