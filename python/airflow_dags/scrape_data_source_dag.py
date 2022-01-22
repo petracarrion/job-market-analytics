96e306e7588ce3d7f320a2516912c0ae3c52d5f4
Petra Carrion <petra@carrion.io>
2022-01-22 20:17:19 +0100

    Refactor

--- python/airflow_dags/scrape_data_source_dag.py
+++ python/airflow_dags/scrape_data_source_dag.py
@@ -1,36 +1,27 @@
 from datetime import datetime
 
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.decorators import task
+from airflow.operators.python import get_current_context
 from airflow.providers.http.hooks.http import HttpHook
-from airflow.providers.http.operators.http import SimpleHttpOperator
 
-dag = DAG('scrape_data_source_dag',
-          description='Scrape Data Source DAG',
-          schedule_interval='0 * * * *',
-          start_date=datetime(2022, 1, 1),
-          catchup=False)
-
-task_check_vpn_status_op = SimpleHttpOperator(
-    task_id='check_vpn_status_task',
-    method='GET',
-    endpoint='do/check-vpn-status',
-    dag=dag,
-)
+with DAG('scrape_data_source_dag',
+         description='Scrape Data Source DAG',
+         schedule_interval='0 * * * *',
+         start_date=datetime(2022, 1, 1),
+         catchup=False) as dag:
+    @task(task_id="check_vpn_status_task")
+    def check_vpn_status_task():
+        HttpHook(method='GET').run('do/check-vpn-status')
 
 
-def task_scrape_data_source_func(ts):
-    http_hook = HttpHook()
-    data = {
-        'ts': ts,
-    }
-    http_hook.run('do/scrape-data-source', data)
+    @task(task_id="scrape_data_source_task")
+    def scrape_data_source_task():
+        context = get_current_context()
+        data = {
+            'ts': context['ts'],
+        }
+        HttpHook().run('do/scrape-data-source', data)
 
 
-task_scrape_data_source_op = PythonOperator(
-    task_id='scrape_data_source_task',
-    python_callable=task_scrape_data_source_func,
-    dag=dag,
-)
-
-task_check_vpn_status_op >> task_scrape_data_source_op
+    check_vpn_status_task() >> scrape_data_source_task()
