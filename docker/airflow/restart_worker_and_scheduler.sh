d3e87439f495eb4e5a75bf3171c2a5c099d038b5
Petra Carrion <petra@carrion.io>
2022-04-20 17:15:44 +0200

    Add script restart_worker_and_scheduler.sh

---
+++ docker/airflow/restart_worker_and_scheduler.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for container in airflow-worker airflow-scheduler; do
+  docker compose stop $container
+  docker compose rm -f $container
+  docker compose up $container &
+done
