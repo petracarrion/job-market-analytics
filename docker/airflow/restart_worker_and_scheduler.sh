844b6339a3b8dc252061f0f007d90365a3d9e206
Petra Carrion <petra@carrion.io>
2022-07-19 22:21:14 +0200

    Fix bug

--- docker/airflow/restart_worker_and_scheduler.sh
+++ docker/airflow/restart_worker_and_scheduler.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd "$SCRIPTPATH" || exit
+
 for container in airflow-worker airflow-scheduler; do
   docker compose stop $container
   docker compose rm -f $container
