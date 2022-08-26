87108f3d8302c28f22f05124144bf265fddd489e
Petra Carrion <petra@carrion.io>
2022-08-26 21:58:02 +0200

    Add backup-day.sh

---
+++ python/simplescraper/backup-day.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source .env
+
+if [[ $# -ne 3 ]] ; then
+    echo "Please provide a date as script parameters in the following format: year month day"
+    echo "Example: $0 2022 12 01"
+    exit 1
+fi
+
+for entity in job_description sitemap
+do
+
+  source=${RAW_DIR}/${DATA_SOURCE_NAME}/${entity}/$1/$2/$3
+
+  if [ -d "$source" ]
+  then
+
+    target_dir=${BACKUP_DIR}/${entity}/$1/$2
+    target_filename=${target_dir}/${entity}.$1$2$3.tar.gz
+    mkdir -p "${target_dir}"
+    tar -zcvf "${target_filename}" -C "${source}" .
+
+  fi
+
+done
