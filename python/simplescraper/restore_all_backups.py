0cb7ce34c74e983dd2164bceb0f507083fe8fa1a
Petra Carrion <petra@carrion.io>
2022-09-09 23:29:13 +0200

    Add restore_all_backups.py

--- python/simplescraper/restore_all_backups.py
+++ python/simplescraper/restore_all_backups.py
@@ -11,43 +11,43 @@ def get_current_date():
     return datetime.datetime.today().strftime('%Y%m%d')
 
 
-def list_days_to_backup(entity):
-    df = pd.DataFrame(list_raw_days(DATA_SOURCE_NAME, entity))
-    df_backup_days = pd.DataFrame(list_backup_days(DATA_SOURCE_NAME, entity))
+def list_backups_to_restore(entity):
+    df = pd.DataFrame(list_backup_days(DATA_SOURCE_NAME, entity))
+    df_in_raw = pd.DataFrame(list_raw_days(DATA_SOURCE_NAME, entity))
     df_current_date = pd.DataFrame([{
         'date': get_current_date()
     }])
     df = df.drop_duplicates()
     df = pd.concat([
         df,
-        df_backup_days, df_backup_days,
+        df_in_raw, df_in_raw,
         df_current_date, df_current_date
     ]).drop_duplicates(keep=False)
     return df
 
 
-def print_script_statements(script_name, dates_to_download):
-    for date_to_download in dates_to_download:
-        year = date_to_download[:4]
-        month = date_to_download[4:6]
-        day = date_to_download[6:8]
+def print_script_statements(script_name, days_to_restore):
+    for day_to_restore in days_to_restore:
+        year = day_to_restore[:4]
+        month = day_to_restore[4:6]
+        day = day_to_restore[6:8]
         print(
             f'/bin/zsh {SOURCE_DIR}/simplescraper/{script_name} {year} {month} {day} || exit 1')
 
 
-def do_all_backups():
+def restore_all_backups():
     dfs = []
     for entity in ALL_ENTITIES:
-        df = list_days_to_backup(entity)
+        df = list_backups_to_restore(entity)
         dfs.append(df)
     df = pd.concat(dfs, ignore_index=True)
     df = df.drop_duplicates()
     df = df.sort_values(by=['date'])
-    dates_to_download = df['date'].to_list()
-    print_script_statements('do_day_backup.sh', dates_to_download)
+    days_to_restore = df['date'].to_list()
+    print_script_statements('restore_day_backup.sh', days_to_restore)
     print()
-    print_script_statements('verify_day_backup.sh', dates_to_download)
+    print_script_statements('verify_day_backup.sh', days_to_restore)
 
 
 if __name__ == "__main__":
-    do_all_backups()
+    restore_all_backups()
