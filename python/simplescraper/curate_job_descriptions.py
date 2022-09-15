70624ef6220dbb20db25cac51caf7d552c7bf1c3
Petra Carrion <petra@carrion.io>
2022-09-15 22:48:12 +0200

    Fix bug

--- python/simplescraper/curate_job_descriptions.py
+++ python/simplescraper/curate_job_descriptions.py
@@ -49,6 +49,7 @@ def curate_job_descriptions(load_timestamp, load_date):
     configure_logger(load_timestamp, 'curate_job_descriptions')
     logger.info(f'Start curate_job_descriptions: {load_date}')
     df = load_cleansed_df(JOB_DESCRIPTION, load_date=load_date)
+    df = df.dropna(subset=['job_id'])
     df = df.sort_values(by=['job_id'])
 
     process_job_description(df)
