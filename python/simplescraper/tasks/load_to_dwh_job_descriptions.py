a1d832b5e996308c5ef829e7d322ab0d06ccde71
Petra Carrion <petra@carrion.io>
2022-09-15 23:01:34 +0200

    Add load_to_dwh_job_descriptions.py

--- python/simplescraper/tasks/load_to_dwh_job_descriptions.py
+++ python/simplescraper/tasks/load_to_dwh_job_descriptions.py
@@ -8,18 +8,11 @@ from common.env_variables import CURATED_DIR, DATA_SOURCE_NAME, DUCKDB_WAREHOUSE
 from common.logging import configure_logger, logger
 from common.storage import get_load_timestamp, get_load_date
 
-CREATE_TABLE_HUB_JOB_DESCRIPTION = '''
-    CREATE TABLE IF NOT EXISTS hub_job_description (
-         job_id NUMERIC
-        ,load_timestamp TIMESTAMP
-        )
-    '''
-
-CREATE_TABLE_SAT_JOB_DESCRIPTION = '''
-    CREATE TABLE IF NOT EXISTS sat_job_description (
+CREATE_TABLE_SRC_JOB_DESCRIPTION = '''
+    CREATE TABLE IF NOT EXISTS src_job_description (
          job_id VARCHAR
-        ,job_hashdiff VARCHAR
         ,load_timestamp TIMESTAMP
+        ,job_hashdiff VARCHAR
         ,title VARCHAR
         ,online_status VARCHAR
         ,is_anonymous BOOLEAN
@@ -37,19 +30,19 @@ CREATE_TABLE_SAT_JOB_DESCRIPTION = '''
 
 def load_to_vault_job_descriptions(load_timestamp, load_date):
     configure_logger(load_timestamp, 'load_to_vault_job_descriptions')
-    logger.info(f'Start load_to_vault_job_descriptions: {load_date}')
+    logger.info(f'Start load_to_dwh_job_descriptions: {load_date}')
 
     conn = duckdb.connect(DUCKDB_WAREHOUSE_FILE)
 
-    conn.execute(CREATE_TABLE_HUB_JOB_DESCRIPTION)
-    conn.execute(CREATE_TABLE_SAT_JOB_DESCRIPTION)
+    conn.execute(CREATE_TABLE_SRC_JOB_DESCRIPTION)
 
     parquet_input = os.path.join(CURATED_DIR, DATA_SOURCE_NAME, 'job_description')
     dataset = ds.dataset(parquet_input, format='parquet', partitioning='hive')
     conn.register('''curated_job_description''', dataset)
     year, month, day = load_date.split('/', 2)
-    df = conn.execute(f'''    
-    CREATE TEMP TABLE staging_job_description AS
+
+    conn.execute(f'''    
+    CREATE TEMP TABLE tmp_job_description AS
         SELECT *
             FROM curated_job_description
             WHERE
@@ -57,20 +50,11 @@ def load_to_vault_job_descriptions(load_timestamp, load_date):
                 month = {month} AND
                 day = {day};
     
-    INSERT INTO hub_job_description 
+    INSERT INTO src_job_description 
         SELECT
              a.job_id
-            ,'{year}-{month}-{day}'
-        FROM staging_job_description a
-        LEFT OUTER JOIN hub_job_description b
-            ON (a.job_id = b.job_id)
-        WHERE b.job_id IS NULL;
-         
-    INSERT INTO sat_job_description 
-        SELECT
-             a.job_id
+            ,a.load_timestamp
             ,a.job_hashdiff
-            ,'{year}-{month}-{day}'
             ,a.title
             ,a.online_status
             ,a.is_anonymous
@@ -82,24 +66,16 @@ def load_to_vault_job_descriptions(load_timestamp, load_date):
             ,a.description_responsabilities
             ,a.description_requirements
             ,a.description_perks
-        FROM staging_job_description a
-        LEFT OUTER JOIN sat_job_description b
+        FROM tmp_job_description a
+        LEFT OUTER JOIN src_job_description b
             ON (
-                a.job_id = b.job_id AND
-                a.job_hashdiff = b.job_hashdiff
+                a.job_id = b.job_id
             )
         WHERE
             b.job_id IS NULL;
-    ''').df()
-    print(df)
-    df = conn.execute(f'''
-    SELECT * FROM hub_job_description;
-    ''').df()
-    print(df)
-    df = conn.execute(f'''
-    SELECT * FROM sat_job_description;
-    ''').df()
-    print(df)
+    ''')
+
+    logger.info(f'End   load_to_dwh_job_descriptions: {load_date}')
 
     conn.close()
 
