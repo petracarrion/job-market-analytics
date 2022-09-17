88fea63e8bd8aad55d40361c4b043dbc15f45d29
Petra Carrion <petra@carrion.io>
2022-09-17 02:44:07 +0200

    Improve concurrency

--- python/simplescraper/common/logging.py
+++ python/simplescraper/common/logging.py
@@ -1,4 +1,5 @@
 import os.path
+import sys
 
 from loguru import logger
 
@@ -6,6 +7,8 @@ from common.env_variables import TEMP_DIR
 
 
 def configure_logger(load_timestamp):
+    logger.remove()
+    logger.add(sys.stdout, colorize=True)
     logger.add(os.path.join(TEMP_DIR, load_timestamp, f'00_logs.log'))
 
 
