2976892a81b65f2b9230c12525c58cb8e5995c9f
Petra Carrion <petra@carrion.io>
2022-09-17 21:18:44 +0200

    Add job_market_analytics_cleanse_sitemaps_catch_up_dag.py and job_market_analytics_curate_sitemaps_catch_up_dag.py

---
+++ python/airflow_dags/job_market_analytics_cleanse_sitemaps_catch_up_dag.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.decorators import task
+
+from common_airflow_dag import run_flasky_task
+
+with DAG('job_market_analytics_cleanse_sitemaps_catch_up_dag',
+         description='Job Market Analytics Cleanse Sitemaps Catch Up DAG',
+         schedule_interval='@daily',
+         start_date=datetime(2022, 1, 1),
+         # end_date=datetime(2021, 12, 1),
+         dagrun_timeout=timedelta(minutes=10),
+         max_active_runs=4,
+         max_active_tasks=4,
+         catchup=True) as dag:
+    @task(task_id="cleanse_sitemaps")
+    def cleanse_sitemaps():
+        run_flasky_task('do/cleanse_sitemaps')
+
+
+    cleanse_sitemaps()
