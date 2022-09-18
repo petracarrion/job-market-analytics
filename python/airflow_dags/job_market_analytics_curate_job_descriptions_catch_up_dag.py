c62322c72affa293669d2547077b48fd508b03f2
Petra Carrion <petra@carrion.io>
2022-09-18 10:17:18 +0200

    Add job_market_analytics_curate_job_descriptions_catch_up_dag.py

---
+++ python/airflow_dags/job_market_analytics_curate_job_descriptions_catch_up_dag.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.decorators import task
+
+from common_airflow_dag import run_flasky_task
+
+with DAG('job_market_analytics_curate_job_descriptions_catch_up_dag',
+         description='Job Market Analytics Curate Job Descriptions Catch Up DAG',
+         schedule_interval='@daily',
+         start_date=datetime(2021, 10, 1),
+         dagrun_timeout=timedelta(minutes=60),
+         max_active_runs=4,
+         max_active_tasks=4,
+         catchup=True) as dag:
+    @task(task_id="curate_job_descriptions")
+    def curate_job_descriptions():
+        run_flasky_task('do/curate_job_descriptions')
+
+
+    curate_job_descriptions()
