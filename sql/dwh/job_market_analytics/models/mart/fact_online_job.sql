0ce0736e2594bcead044e51d45258eeb907e24f8
Petra Carrion <petra@carrion.io>
2022-09-24 17:56:45 +0200

    Add dbt model fact_online_job.sql

---
+++ sql/dwh/job_market_analytics/models/mart/fact_online_job.sql
@@ -0,0 +1,19 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
+
+SELECT online_at     AS date_id,
+       COUNT(job_id) AS total
+FROM {{ source('curated', 'job_online') }} {% if is_incremental() %}
+WHERE online_at
+    > (
+    SELECT max(date_id)
+    FROM {{ this }}
+    )
+    {% endif %}
+
+GROUP BY 1
+ORDER BY 1
