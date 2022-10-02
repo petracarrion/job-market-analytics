888344e6ebe5744ddc6d154fcd0a228694db83af
Petra Carrion <petra@carrion.io>
2022-10-02 20:36:31 +0200

    Add dim_time.sql

--- sql/dwh/job_market_analytics/models/mart/dim_time.sql
+++ sql/dwh/job_market_analytics/models/mart/dim_time.sql
@@ -1,15 +1,20 @@
 {{
-  config(
-    materialized = 'table',
+    config(
+        materialized = 'table',
     )
 }}
 
-SELECT range as date_key,
-       date_part('year', range) as year,
-       date_part('month', range) as month,
-       date_part('day', range) as day,
-       monthname(range) as month_name,
-       date_part('yearweek', range) as year_week,
-       date_part('isodow', range) as day_of_week,
-       dayname(range) as day_of_week_name
-  FROM range(TIMESTAMP '2021-01-01', TIMESTAMP '2023-01-01', INTERVAL 1 DAY)
+WITH unique_online_at AS (
+    SELECT DISTINCT online_at
+      FROM {{ source('curated', 'online_job') }}
+     ORDER BY 1
+)
+SELECT online_at as date_key,
+       date_part('year', online_at) as year,
+       date_part('month', online_at) as month,
+       date_part('day', online_at) as day,
+       monthname(online_at) as month_name,
+       date_part('yearweek', online_at) as year_week,
+       date_part('isodow', online_at) as day_of_week,
+       dayname(online_at) as day_of_week_name
+  FROM unique_online_at
