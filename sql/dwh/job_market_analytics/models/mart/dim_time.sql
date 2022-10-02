23f6e4fc692103895df5814d2edbb1faf31a265c
Petra Carrion <petra@carrion.io>
2022-10-02 17:30:57 +0200

    Add dim_time.sql

--- sql/dwh/job_market_analytics/models/mart/dim_time.sql
+++ sql/dwh/job_market_analytics/models/mart/dim_time.sql
@@ -8,8 +8,8 @@ SELECT range as date_key,
        date_part('year', range) as year,
        date_part('month', range) as month,
        date_part('day', range) as day,
-       monthname(range) as monthname,
-       date_part('yearweek', range) as yearweek,
-       date_part('isodow', range) as isodow,
-       dayname(range) as dayname
+       monthname(range) as month_name,
+       date_part('yearweek', range) as year_week,
+       date_part('isodow', range) as day_of_week,
+       dayname(range) as day_of_week_name
   FROM range(TIMESTAMP '2021-01-01', TIMESTAMP '2023-01-01', INTERVAL 1 DAY)
