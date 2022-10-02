f9a2bd477818ac2f410245e38489183a55d694a7
Petra Carrion <petra@carrion.io>
2022-10-02 20:26:17 +0200

    Add dim_job.sql

---
+++ sql/dwh/job_market_analytics/models/mart/dim_job.sql
@@ -0,0 +1,35 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
+
+SELECT MD5(CONCAT_WS('||',
+            COALESCE(
+                job.job_id,
+                UPPER(TRIM(CAST(
+                    job.job_id
+                AS VARCHAR))),
+                NULL,
+                '^^'),
+            COALESCE(
+                job.load_timestamp,
+                UPPER(TRIM(CAST(
+                    job.load_timestamp
+                AS VARCHAR))),
+                NULL,
+                '^^')
+       )) AS job_key,
+       job.job_id,
+       job.load_timestamp as job_ldts,
+       job.title,
+       job.company_name
+  FROM {{ source('curated', 'job') }}
+
+{% if is_incremental() %}
+ RIGHT OUTER JOIN dim_job
+  ON (job.job_id   = dim_job.job_id AND
+      job.load_timestamp = dim_job.job_ldts)
+WHERE job.job_id IS NULL
+{% endif %}
