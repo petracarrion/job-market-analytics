9bb997e00c61b8ef1c3b4e582aea4a462331bcdf
Petra Carrion <petra@carrion.io>
2022-10-07 19:57:02 +0200

    Add dashy.py

--- python/dashy/dashy.py
+++ python/dashy/dashy.py
@@ -1,15 +1,16 @@
 import os
 
+import dash_bootstrap_components as dbc
 import duckdb
 import plotly.express as px
-from dash import Dash, dcc, html, Output, Input, ctx
+from dash import Dash, dcc, html, Output, Input
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DUCKDB_DWH_FILE = os.getenv('DUCKDB_DWH_FILE')
 
-app = Dash('Dashy', title='Job Market Analytics')
+app = Dash('Dashy', title='Job Market Analytics', external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 
 @app.callback(Output('main-graph', 'children'),
@@ -23,18 +24,6 @@ def get_main_graph(time_input, location_input, company_input):
     location_clause = f'location_name IN (SELECT UNNEST({location_input}))' if location_input else '1 = 1'
     company_clause = f'company_name IN (SELECT UNNEST({company_input}))' if company_input else '1 = 1'
 
-    # if location_input:
-    #     joined_locations = ', '.join(location_input)
-    #     location_clause = f'location_name in ({joined_locations})'
-    # else:
-    #     location_clause = '1 = 1'
-    #
-    # if company_input:
-    #     joined_companies = ', '.join(company_input)
-    #     company_clause = f'location_name in ({joined_companies})'
-    # else:
-    #     company_clause = '1 = 1'
-
     df = _conn.execute(f'''
     SELECT online_at,
            SUM(total_jobs) AS total_jobs
@@ -43,7 +32,7 @@ def get_main_graph(time_input, location_input, company_input):
            {location_clause} AND
            {company_clause}
      GROUP BY 1
-    ORDER BY 1
+     ORDER BY 1
     ''').df()
 
     _conn.close()
@@ -59,7 +48,6 @@ def get_main_graph(time_input, location_input, company_input):
 
 
 if __name__ == '__main__':
-
     conn = duckdb.connect(DUCKDB_DWH_FILE, read_only=True)
 
     df_top_cities = conn.execute('''
@@ -67,7 +55,7 @@ if __name__ == '__main__':
            SUM(total_jobs) AS total_jobs
       FROM aggregated_online_job
      GROUP BY 1
-    ORDER BY 2 DESC
+     ORDER BY 2 DESC
     LIMIT 100
     ''').df()
 
@@ -76,13 +64,13 @@ if __name__ == '__main__':
            SUM(total_jobs) AS total_jobs
       FROM aggregated_online_job
      GROUP BY 1
-    ORDER BY 2 DESC
+     ORDER BY 2 DESC
     LIMIT 100
     ''').df()
 
     conn.close()
 
-    time_selector = dcc.RadioItems(
+    time_selector = dbc.RadioItems(
         options=[
             {'label': 'Last Year', 'value': '12'},
             {'label': 'Last 6 Months', 'value': '6'},
@@ -94,33 +82,48 @@ if __name__ == '__main__':
 
     location_options = df_top_cities['location_name'].to_list()
     location_options = [{'label': location, 'value': location} for location in location_options]
-    location_selector = dcc.Checklist(
+    location_selector = dbc.Checklist(
         options=location_options,
         id='location-selector'
     )
 
     company_options = df_top_companies['company_name'].to_list()
     company_options = [{'label': company, 'value': company} for company in company_options]
-    company_selector = dcc.Checklist(
+    company_selector = dbc.Checklist(
         options=company_options,
         id='company-selector'
     )
 
-    app.layout = html.Div([
-        html.H1('Job Market Analytics'),
-        html.Div([
-            html.H3('Time Range'),
-            time_selector
-        ]),
-        html.Div([
-            html.H3('City'),
-            location_selector
-        ]),
-        html.Div([
-            html.H3('Company'),
-            company_selector
-        ]),
-        html.Div(id='main-graph')
-    ])
+    controls = dbc.Card(
+        [
+            html.Div([
+                html.H2("Time Range"),
+                time_selector,
+            ]),
+            html.Div([
+                html.H2('City'),
+                location_selector
+            ]),
+            html.Div([
+                html.H2('Company'),
+                company_selector
+            ]),
+        ],
+        body=True,
+    )
+
+    app.layout = dbc.Container(
+        [
+            html.H1("Job Market Analytics"),
+            html.Hr(),
+            dbc.Row(
+                [
+                    dbc.Col(controls, md=4),
+                    dbc.Col(html.Div(id='main-graph'), md=8),
+                ],
+            ),
+        ],
+        fluid=True,
+    )
 
     app.run_server(debug=True)
