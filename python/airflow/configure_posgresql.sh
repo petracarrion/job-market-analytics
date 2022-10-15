2443705ed66bc97f8312e9a50beb27186a77a99e
Petra Carrion <petra@carrion.io>
2022-10-15 09:13:50 +0200

    Add configure_posgresql.sh

---
+++ python/airflow/configure_posgresql.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd "$SCRIPTPATH" || exit
+
+source .env
+
+sudo -u postgres psql -c "CREATE DATABASE ${AIRFLOW_DATABASE_NAME};"
+
+sudo -u postgres psql -c "CREATE USER ${AIRFLOW_DATABASE_USERNAME} WITH ENCRYPTED PASSWORD '${AIRFLOW_DATABASE_PASSWORD};'"
+
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${AIRFLOW_DATABASE_NAME} TO ${AIRFLOW_DATABASE_USERNAME};"
