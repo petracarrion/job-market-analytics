d96327e27769f09edcce4aca5d4ed0b4a46ef12a
Petra Carrion <petra@carrion.io>
2022-12-02 09:33:47 +0100

    Fix bug

--- python/airflow/airflow_home/dags/job_market_analytics_curate_catch_up_dag.py
+++ python/airflow/airflow_home/dags/job_market_analytics_curate_catch_up_dag.py
@@ -8,13 +8,17 @@ from common_airflow_dag import run_flasky_task
 
 os.environ["no_proxy"] = "*"
 
+YEAR = 2021
+MONTH = 10
+
 with DAG('job_market_analytics_curate_catch_up_dag',
          description='Job Market Analytics Curate Catch Up DAG',
          schedule_interval='@daily',
-         start_date=datetime(2021, 10, 1),
+         start_date=datetime(YEAR, MONTH, 1),
+         end_date=datetime(YEAR, MONTH, 1) + timedelta(days=30),
          dagrun_timeout=timedelta(minutes=60),
-         max_active_runs=4,
-         max_active_tasks=4,
+         max_active_runs=2,
+         max_active_tasks=2,
          catchup=True) as dag:
     @task(task_id="curate_sitemaps")
     def curate_sitemaps():
