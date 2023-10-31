0bd216a63ec6b530b67e9be0ad3083c727bafa07
Petra Carrion <petra@carrion.io>
2023-10-31 20:43:05 +0100

    Upgrade Airflow

--- python/airflow/install_airflow.sh
+++ python/airflow/install_airflow.sh
@@ -3,13 +3,15 @@
 which pip | grep /airflow/venv/ || (echo "Wrong venv!!!" && exit)
 
 # Install Airflow using the constraints file
-AIRFLOW_VERSION=2.4.1
+AIRFLOW_VERSION=2.7.2
 PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 # For example: 3.7
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 # For example: https://raw.githubusercontent.com/apache/airflow/constraints-2.4.1/constraints-3.7.txt
 pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
 pip install psycopg2
+
+airflow db upgrade
 
 # The Standalone command will initialise the database, make a user,
 # and start all components for you.
